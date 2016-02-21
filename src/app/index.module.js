2f165793751c7a5b5259e4d15fba53cd51e00b5e
Ivan Khlebushkin <Ivan_Khlebushkin@epam.com>
2016-02-22 00:27:14 +0600

    Add Test

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('testAngular', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,19 @@
+angular
+    .module('testAngular')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($scope, $timeout, webDevTec, toastr) {        
+    $scope.someHTML = "<p>Fill out the form you need</p>";  
+    
+    $scope.someArray = [
+        {name: 'Form - ', value: "View 1", isDoc: 0, titleForm:"", textForm:""},
+        {name: 'Form - ', value: "View 2", isDoc: 1, titleForm:"Statement", textForm:"Please fire me on their own. Regards,"},
+        {name: 'Form - ', value: "View 3", isDoc: 0, titleForm:"", textForm:""},
+        ];
+    
+    $scope.SelectDoc = function(item) {
+        $scope.formTitle = item.titleForm;
+        $scope.formText = item.textForm;
+    };
+  }
